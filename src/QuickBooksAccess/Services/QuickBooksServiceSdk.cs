75a61d604e30565db360402d8c363f1941ace307
MaxKits <macros5@ya.ru>
2014-10-07 23:07:28 +0400

    added qb sdk service

---
+++ src/QuickBooksAccess/Services/QuickBooksServiceSdk.cs
@@ -0,0 +1,15 @@
+﻿namespace QuickBooksAccess.Services
+{
+	internal class QuickBooksServiceSdk
+	{
+		public UpdateInventoryResponse UpdateInventory()
+		{
+			var result = new UpdateInventoryResponse();
+			return result;
+		}
+	}
+
+	internal class UpdateInventoryResponse
+	{
+	}
+}
