93a62c3c32c059da969e4fd2bd55d6dc9a39fe89
MaxKits <macros5@ya.ru>
2014-10-21 01:42:32 +0400

    added QuickBooksNonAuthenticatedUserCredentials

---
+++ src/QuickBooksAccess/Models/QuickBooksNonAuthenticatedUserCredentials.cs
@@ -0,0 +1,54 @@
+﻿namespace QuickBooksAccess.Models
+{
+	public class QuickBooksNonAuthenticatedUserCredentials
+	{
+		public QuickBooksNonAuthenticatedUserCredentials(
+			string appToken,
+			string consumerKey,
+			string consumerSecret,
+			string callBackUrl,
+			string oauthEndPoint,
+			string getRequestTokenUrl,
+			string getAccessTokenUrl,
+			string authorizeUrl )
+		{
+			this.AppToken = appToken;
+			this.ConsumerKey = consumerKey;
+			this.ConsumerSecret = consumerSecret;
+			this.CallbackUrl = callBackUrl;
+			this.OauthEndPoint = oauthEndPoint;
+			this.GetRequestTokenUrl = getRequestTokenUrl;
+			this.GetAccessTokenUrl = getAccessTokenUrl;
+			this.AuthorizeUrl = authorizeUrl;
+		}
+
+		public QuickBooksNonAuthenticatedUserCredentials(
+			string appToken,
+			string consumerKey,
+			string consumerSecret,
+			string callBackUrl,
+			string oauthEndPoint = "https://oauth.intuit.com/oauth/v1",
+			string authorizeUrl = "https://workplace.intuit.com/Connect/Begin" )
+			: this(
+				appToken,
+				consumerKey,
+				consumerSecret,
+				callBackUrl,
+				oauthEndPoint,
+				oauthEndPoint + "/get_request_token",
+				oauthEndPoint + "/get_access_token",
+				authorizeUrl
+				)
+		{
+		}
+
+		public string AppToken { get; private set; }
+		public string ConsumerKey { get; private set; }
+		public string ConsumerSecret { get; private set; }
+		public string CallbackUrl { get; private set; }
+		public string OauthEndPoint { get; private set; }
+		public string GetRequestTokenUrl { get; private set; }
+		public string GetAccessTokenUrl { get; private set; }
+		public string AuthorizeUrl { get; private set; }
+	}
+}
