ff1746091fa427457bf18f8b965a16c9a125f28e
MaxKits <macros5@ya.ru>
2014-10-30 01:36:24 +0300

    global assembly version added

--- src/QuickBooksOnlineAccessTestsIntegration/Properties/AssemblyInfo.cs
+++ src/QuickBooksOnlineAccessTestsIntegration/Properties/AssemblyInfo.cs
@@ -5,35 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[ assembly : AssemblyTitle( "QuickBooksAccessTestsIntegration" ) ]
-[ assembly : AssemblyDescription( "" ) ]
+[assembly: AssemblyTitle("QuickBooksOnlineAccessTestsIntegration")]
 [ assembly : AssemblyConfiguration( "" ) ]
-[ assembly : AssemblyCompany( "" ) ]
-[ assembly : AssemblyProduct( "QuickBooksAccessTestsIntegration" ) ]
-[ assembly : AssemblyCopyright( "Copyright ©  2014" ) ]
-[ assembly : AssemblyTrademark( "" ) ]
-[ assembly : AssemblyCulture( "" ) ]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[ assembly : ComVisible( false ) ]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [ assembly : Guid( "0828cdb8-77f1-4fbf-b9c8-0e89ba67009b" ) ]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[ assembly : AssemblyVersion( "1.0.0.0" ) ]
-[ assembly : AssemblyFileVersion( "1.0.0.0" ) ]
